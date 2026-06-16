3279de2c4090d07fd28936799abedcf6bde34de7
alzulaikha <alzulaikha1992@gmail.com>
2026-06-16 08:31:39 +0400

    Added classes and attributes

--- oop_part1/Program.cs
+++ oop_part1/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace oop_part1
+﻿using System.Xml.Linq;
+
+namespace oop_part1
 {
+    public class Room
+    {
+
+        public int roomNumber;
+        public string roomType {  get; set; }
+        public double pricePerNight {get;set;}
+        public bool isAvailable { get;set;}
+    }
+    public class Guest
+    {
+
+        public int guestId;
+        public string guestName { get;set;}
+        public string roomNumber {  get;set;}
+        public string checkInDate { get;set;}
+        public string totalNights { get;set;}
+ }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
